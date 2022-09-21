d91624b4ed9b42928966e3c0aa90d7491813b579
abdullah.umar <abdullah.umar@axiatadigitallabs.com>
2022-09-22 00:16:35 +0700

    add express server package

---
+++ server.js
@@ -0,0 +1,19 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+/* eslint-disable no-undef */
+//Install express server
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const express = require('express');
+
+const path = require('path');
+
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function (req, res) {
+	res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
